7225dd2a14624b370a189699af90052a856191aa
Luciano <lucianobenjaminpm@gmail.com>
2023-08-30 13:31:58 -0400

    csv integrateed

---
+++ src/components.rs
@@ -0,0 +1,85 @@
+use std::fs::File;
+use csv::ReaderBuilder;
+use dioxus::prelude::*;
+use dioxus_router::prelude::*;
+use crate::Route;
+
+type Records = (String, String, u8);
+
+#[inline_props]
+pub fn NavBar(cx: Scope) -> Element{
+  render!{
+    div{
+      class:"navbar",
+      Link{
+        to:Route::Home {},
+        "Push"
+      }
+      Link{
+        to:Route::Push {},
+        "Pull"
+      }  
+    }
+    Outlet::<Route> {}
+  }
+}
+#[inline_props]
+pub fn Home(cx: Scope) -> Element{
+  let file =  match File::open("data.csv"){
+    Ok(file) => file,
+    Err(_) => {
+      File::create("data.csv").unwrap();
+      println!("created");
+      File::open("data.csv").unwrap()
+    }
+  };
+  let mut rdr = ReaderBuilder::new()
+    .flexible(true)
+    .comment(Some(b'#'))
+    .from_reader(file);
+  for result in rdr.deserialize(){
+    let record:Records = result.unwrap();
+    println!("{:?}",record)
+  }
+  render!{
+    "Home"
+  }
+}
+#[inline_props]
+pub fn Push(cx: Scope) -> Element{
+  let file =  match File::open("data.csv"){
+    Ok(file) => file,
+    Err(_) => {
+      File::create("data.csv").unwrap();
+      println!("created");
+      File::open("data.csv").unwrap()
+    }
+  };
+  let mut rdr = ReaderBuilder::new()
+    .flexible(true)
+    .comment(Some(b'#'))
+    .from_reader(file);
+  for result in rdr.deserialize(){
+    let record:Records = result.unwrap();
+    println!("{:?}",record)
+  }
+  let par = use_state(cx, ||("".to_string(), "".to_string(), "".to_string()));  
+  render!{
+    "Push"
+    form{
+      input{
+        r#type:"text",
+
+      }
+      input{
+        r#type:"submit"
+      }
+    }
+  }
+}
+#[inline_props]
+pub fn NotFound(cx: Scope) -> Element{
+  render!{
+    "Not Found"
+  }
+}
